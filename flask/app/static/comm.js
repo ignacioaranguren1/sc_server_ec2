9d68f008169bf04d7e89abd3ab5ec5fee82c38e2
ignacioaranguren1 <ignacio.aranguren21@imperial.ac.uk>
2022-04-12 00:55:19 +0100

    layout for mobile devices improved; more specific settings still need to be included

--- flask/app/static/comm.js
+++ flask/app/static/comm.js
@@ -31,7 +31,7 @@ function send_response(id_str, sentiment) {
 
 function update_content(){
     let req = new XMLHttpRequest();
-        req.open('GET', 'http://127.0.0.1:5000/update_news', true);
+        req.open('GET', 'http://'.concat(location.hostname).concat(":").concat(location.port).concat('/update_news'), true);
         req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         req.send();
         req.onreadystatechange = function (aEvt) {
