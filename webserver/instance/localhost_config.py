9e30bc1fe39f72ae42d4fe9b2135d4dc03e7fbeb
ignacioaranguren1 <ignacio.aranguren21@imperial.ac.uk>
2022-04-21 19:29:20 +0100

    final version revised

--- webserver/instance/localhost_config.py
+++ webserver/instance/localhost_config.py
@@ -1,5 +1,6 @@
 DB_USERNAME = "sin_chan"
 DB_PASSWORD = "_sinosuke_noara"
 DB_NAME = "sentiment"
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + DB_USERNAME + ":" + DB_PASSWORD + "@localhost/" + DB_NAME + "?charset=utf8mb4"
+MYSQL_DATABASE_CHARSET = 'utf8mb4'
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + DB_USERNAME + ":" + DB_PASSWORD + "@localhost/" + DB_NAME
 SQLALCHEMY_TRACK_MODIFICATIONS = True
