71af080379c0121acb20d69da668b549236d54a2
ignacioaranguren1 <ignacio.aranguren21@imperial.ac.uk>
2022-04-21 19:33:23 +0100

    final version revised

--- webserver/instance/docker_config.py
+++ webserver/instance/docker_config.py
@@ -1,6 +1,5 @@
 DB_USERNAME = "sin_chan"
 DB_PASSWORD = "_sinosuke_noara"
 DB_NAME = "sentiment"
-MYSQL_DATABASE_CHARSET = 'utf8mb4'
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + DB_USERNAME + ":" + DB_PASSWORD + "@mysql:3306/" + DB_NAME
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://" + DB_USERNAME + ":" + DB_PASSWORD + "@mysql:3306/" + DB_NAME + "?charset=utf8mb4"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
